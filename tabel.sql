9c68c55f162a5b875aca1722c234664eb2f077cf
Indira Rully Pricilia <92698567+Indirarully@users.noreply.github.com>
2024-07-23 16:28:14 +0700

    Add files via upload

---
+++ tabel.sql
@@ -0,0 +1,6 @@
+CREATE TABLE items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama_barang VARCHAR(100) NOT NULL
+);
+
+INSERT INTO items (nama_barang) VALUES ('Laptop'), ('Monitor'), ('Keyboard');
